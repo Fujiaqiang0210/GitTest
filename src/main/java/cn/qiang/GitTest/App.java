1bdaf83c05a58f2da39e7eedce2c7167357dc5de
qiang <1464125340@qq.com>
2021-03-24 20:58:05 +0800

    first commit

---
+++ src/main/java/cn/qiang/GitTest/App.java
@@ -0,0 +1,13 @@
+package cn.qiang.GitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
